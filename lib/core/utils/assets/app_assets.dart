df056fca5f0d1bddc9c231578494eb6188dd2a72
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-01-28 18:49:24 +0200

    feat: add images, AppImages class, and flutter_svg support

--- lib/core/utils/assets/app_assets.dart
+++
@@ -1,7 +0,0 @@
-class AppAssets {
-  AppAssets._();
-  // static const String logo = 'assets/images/logo.png';
-  // static const String arrowBottom = 'assets/icons/arrow_bottom.png';
-  // static const String successIcon = 'assets/icons/succes_icon.png';
-  // ... other assets
-}
