e68a58b4d1720d60955205d3db2a2a8bfbeefc4c
Nadia <nadiaashraff220@gmail.com>
2026-01-28 15:10:26 +0200

    feat: create a reusable GeneralArrowBack widget

--- lib/core/widgets/general_arrow_back.dart
+++ lib/core/widgets/general_arrow_back.dart
@@ -1,1 +1,25 @@
 //todo shared custom arrow back widget used in the app
+import 'package:flutter/material.dart';
+
+class GeneralArrowBack extends StatelessWidget {
+  const GeneralArrowBack({super.key, required this.color});
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 28,
+      height: 28,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        border: Border.all(color: color),
+      ),
+      child: Icon(
+        size: 20,
+        Icons.arrow_back_rounded,
+        color: color,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
