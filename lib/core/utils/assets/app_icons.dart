544f733754ca7a752328f83d925b2814e8af0690
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-01-29 15:00:39 +0200

    feat: add icons, AppIcons class.

---
+++ lib/core/utils/assets/app_icons.dart
@@ -0,0 +1,29 @@
+class AppIcons {
+  AppIcons._();
+
+  static const String iconsAdd = 'assets/icons/add.svg';
+  static const String iconsArrowBack = 'assets/icons/arrow_back.svg';
+  static const String iconsArrowNextQuestion = 'assets/icons/arrow_next_question.svg';
+  static const String iconsClosedEye = 'assets/icons/closed_eye.svg';
+  static const String iconsDownload = 'assets/icons/download.svg';
+  static const String iconsEditMaterial = 'assets/icons/edit_material.svg';
+  static const String iconsEditStudentGrades = 'assets/icons/edit_student_grades.svg';
+  static const String iconsLogo = 'assets/icons/logo.svg';
+  static const String iconsMcqQuestion = 'assets/icons/mcq_question.svg';
+  static const String iconsMenu = 'assets/icons/menu.svg';
+  static const String iconsMore = 'assets/icons/more.svg';
+  static const String iconsNotifibationOutline = 'assets/icons/notifibation_outline.svg';
+  static const String iconsNotificationFill = 'assets/icons/notification_fill.svg';
+  static const String iconsOpenedEyeVisible = 'assets/icons/opened_eye_visible.svg';
+  static const String iconsPdfMaterials = 'assets/icons/pdf_materials.svg';
+  static const String iconsPerson = 'assets/icons/person.svg';
+  static const String iconsPlusFloatingButton = 'assets/icons/plus_floating_button.svg';
+  static const String iconsProfile = 'assets/icons/profile.svg';
+  static const String iconsProfileEmail = 'assets/icons/profile_email.svg';
+  static const String iconsProfileId = 'assets/icons/profile_id.svg';
+  static const String iconsProfileLogout = 'assets/icons/profile_logout.svg';
+  static const String iconsProfileName = 'assets/icons/profile_name.svg';
+  static const String iconsQuestionTimer = 'assets/icons/question_timer.svg';
+  static const String iconsVideoMaterial = 'assets/icons/video_material.svg';
+
+}
