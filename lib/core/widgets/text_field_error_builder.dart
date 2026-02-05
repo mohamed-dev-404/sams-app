f512bab317263ef76b220ce568b03af8074e99d4
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-05 17:18:38 +0200

    chore(widgets): implement custom TextFieldErrorBuilder for displaying custom error widget in text field error case

---
+++ lib/core/widgets/text_field_error_builder.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sams_app/core/utils/assets/app_icons.dart';
+import 'package:sams_app/core/utils/colors/app_colors.dart';
+import 'package:sams_app/core/widgets/svg_icon.dart';
+
+class TextFieldErrorBuilder extends StatelessWidget {
+  final String errorMessage;
+
+  const TextFieldErrorBuilder({
+    required this.errorMessage,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(
+            top: 2,
+          ), // Tiny offset to align icon with first line of text
+          child: SvgIcon(
+            AppIcons.iconsTextFieldError,
+            color: AppColors.red,
+          ),
+        ),
+        const SizedBox(width: 4),
+        Expanded(
+          child: Text(
+            errorMessage,
+            maxLines: null,
+            softWrap: true,
+            textAlign: TextAlign.start,
+          ),
+        ),
+      ],
+    );
+  }
+}
