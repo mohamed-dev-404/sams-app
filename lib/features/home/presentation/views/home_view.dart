5a91805bb823f755520eac97cfe823babd3de1f3
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-17 11:01:27 +0200

    feat(home): implement main home view.

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sams_app/core/widgets/adaptive_layout.dart';
+import 'package:sams_app/features/home/presentation/views/layouts/mobile_home_view.dart';
+import 'package:sams_app/features/home/presentation/views/layouts/web_home_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AdaptiveLayout(
+      mobileLayout: (context) => const MobileHomeView(),
+      webLayout: (context) => const WebHomeView(),
+    );
   }
 }
