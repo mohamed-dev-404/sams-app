0b027239fec8033b73af6d73cc44d01334de2e6f
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 06:51:38 +0200

    chore: add home endpoints to constants.

--- lib/core/utils/constants/api_endpoints.dart
+++ lib/core/utils/constants/api_endpoints.dart
@@ -2,15 +2,16 @@ class EndPoints {
   EndPoints._();
 
   //? EndPoints
-  // static String volumes = 'volumes';
-  // static String logIn = 'auth/login';
-  // static String register = 'auth/register';
-  // static String forgetpassword = 'auth/forgotten-password';
-  // static String virfyOTP = 'auth/verify-token';
-  // static String resetPassword = 'auth/reset-password';
-  // static String addProduct = 'admin/products';
-  // static String uploadProductImageEndPoint(id) {
-  //   return 'admin/products/$id/image';
-  // }
-  // ... more endpoints
+
+   //! --- Home  --- ;
+   //* Instructor endpoints
+  static const String createCourse = 'instructor/courses';
+  static const String getMyCreatedCourses = 'instructor/courses/me';
+
+  //* Enrollment endpoints
+  static const String getMyJoinedCourses = 'enrollments/me';
+  static const String joinCourse = 'enrollments'; 
+  static String unenrollCourse(String courseId) =>
+      'enrollments/my-courses/$courseId';
+
 }
