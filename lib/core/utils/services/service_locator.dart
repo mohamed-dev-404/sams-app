ce849114509721044afc9bbcca0c9ccf25636c9f
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 14:11:40 +0200

    chore(): Sets up the service locator by registering the HomeRepo.

--- lib/core/utils/services/service_locator.dart
+++ lib/core/utils/services/service_locator.dart
@@ -2,13 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sams_app/core/network/api_consumer.dart';
 import 'package:sams_app/core/network/dio_consumer.dart';
+import 'package:sams_app/features/home/data/repos/home_repo.dart';
+import 'package:sams_app/features/home/data/repos/home_repo_impl.dart';
 
 GetIt getIt = GetIt.instance;
 
 void setupServiceLocator() {
   getIt.registerSingleton<ApiConsumer>(DioConsumer(Dio()));
-  // getIt.registerSingleton<HomeRepoImpl>(
-  //   HomeRepoImpl(apiService: getIt.get<ApiService>()),
-  // );
+
+//! register HomeRepo
+  getIt.registerLazySingleton<HomeRepo>(
+    () => HomeRepoImpl( api:getIt<ApiConsumer>()),
+  );
+
   //todo register other services and repositories as needed...
 }
