1ca84201591d4ca0f3ca5aa4aff03e797336c2eb
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 14:43:41 +0200

    chore: define unified responsive routes in AppRouter for home and create course view.

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sams_app/core/utils/router/build_route.dart';
 import 'package:sams_app/core/utils/router/routes_name.dart';
-import 'package:sams_app/core/widgets/general_error_page.dart';
+import 'package:sams_app/features/home/presentation/views/create_course_view.dart';
+import 'package:sams_app/features/home/presentation/views/home_view.dart';
 
 class AppRouter {
   AppRouter._();
@@ -12,25 +14,18 @@ class AppRouter {
   static final appRouter = GoRouter(
     navigatorKey: navigatorKey,
     initialLocation: RoutesName.splash,
-    errorBuilder: (context, state) => const GeneralErrorPage(),
+    errorBuilder: (context, state) => const HomeView(),
     routes: [
-      /// buildRoute(
-      ///   name: RoutesName.splash,
-      ///   path: RoutesName.splash,
-      ///   builder: (context, state) => const SplashView(),
-      /// ),
-      /// buildRoute(
-      ///   name: RoutesName.homeView,
-      ///   path: RoutesName.homeView,
-      ///   builder: (context, state) {
-      ///     final args = state.extra as UserModel;
-      ///    return BlocProvider(
-      ///       create: (context) => sl<HomeCubit>(),
-      ///       child: Homeview(args: args),
-      ///     );
-      ///   },
-      ///),
-      //... more buildRoute calls
+      buildRoute(
+        name: RoutesName.homeView,
+        path: RoutesName.homeView,
+        builder: (context, state) => const HomeView(),
+      ),
+      buildRoute(
+        name: RoutesName.createCourseView,
+        path: RoutesName.createCourseView,
+        builder: (context, state) => const CreateCourseView(),
+      ),
     ],
   );
 }
--- lib/core/utils/router/routes_name.dart
+++ lib/core/utils/router/routes_name.dart
@@ -2,9 +2,9 @@ class RoutesName {
   RoutesName._();
 
   static const String splash = '/splash';
-  // static const String login = '/login';
-  // static const String register = '/register';
-  // static const String otp = '/otp';
-  // static const String home = '/home';
-  // ... other routes
+  
+  // Home
+  static const String createCourseView = '/CreateCourseView';
+  static const String homeView = '/HomeView';
+  
 }
