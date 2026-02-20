9b3299ba3b91de67509040d47eb608bd94576fca
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 21:22:06 +0200

    feat: add manual access token.

--- lib/core/network/interceptors/auth_interceptor.dart
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -1,31 +1,64 @@
+// import 'package:dio/dio.dart';
+
+// class AuthInterceptor extends Interceptor {
+//   final Dio dio;
+
+//   AuthInterceptor(this.dio);
+
+//   @override
+//   Future onError(
+//     DioException err,
+//     ErrorInterceptorHandler handler,
+//   ) async {
+//     if (err.response?.statusCode == 401) {
+//       final newToken = await _refreshToken();
+
+//       if (newToken != null) {
+//         err.requestOptions.headers['Authorization'] = 'Bearer $newToken';
+
+//         final response = await dio.fetch(err.requestOptions);
+//         return handler.resolve(response);
+//       }
+//     }
+
+//     return handler.next(err);
+//   }
+
+//   Future<String?> _refreshToken() async {
+//     final response = await dio.post('auth/refresh');
+//     return response.data['token'];
+//   }
+// }
+
+
 import 'package:dio/dio.dart';
 
 class AuthInterceptor extends Interceptor {
   final Dio dio;
-
   AuthInterceptor(this.dio);
 
   @override
-  Future onError(
-    DioException err,
-    ErrorInterceptorHandler handler,
-  ) async {
-    if (err.response?.statusCode == 401) {
-      final newToken = await _refreshToken();
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    const String manualAccessToken =
 
-      if (newToken != null) {
-        err.requestOptions.headers['Authorization'] = 'Bearer $newToken';
 
-        final response = await dio.fetch(err.requestOptions);
-        return handler.resolve(response);
-      }
-    }
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OThjNTM1ZWM5NWZkNjVmMzYyOTE1MGUiLCJhY2FkZW1pY0VtYWlsIjoiMjAyMjA3MDAwQG82dS5lZHUuZWciLCJyb2xlcyI6WyJpbnN0cnVjdG9yIl0sImlhdCI6MTc3MTYxNDY3MiwiZXhwIjoxNzcxNjE1NTcyfQ.0AGOYy7aCcDz6TO17hXkPzyqtywWex__WJ_FzIObpMA";
 
-    return handler.next(err);
+
+
+
+
+    options.headers['Authorization'] = 'Bearer $manualAccessToken';
+
+    print("🔑 Auth Token Injected Manually");
+    return handler.next(options);
   }
 
-  Future<String?> _refreshToken() async {
-    final response = await dio.post('auth/refresh');
-    return response.data['token'];
+  @override
+  Future onError(DioException err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      print("⚠️ Token Expired or Invalid (401)");
+    }
+    return handler.next(err);
   }
 }
