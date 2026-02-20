fa7171ad39045910218a075794835df6916e076e
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 21:47:39 +0200

    refactor: fix sliver rendering and update home layout.

--- lib/features/home/presentation/views/widgets/course_sliver_list.dart
+++ lib/features/home/presentation/views/widgets/course_sliver_list.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:sams_app/features/home/presentation/views/widgets/enum_user_role.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sams_app/core/enums/enum_user_role.dart';
+import 'package:sams_app/core/widgets/app_animated_loading_indicator.dart';
+import 'package:sams_app/features/home/presentation/view_models/cubit/home_cubit.dart';
+import 'package:sams_app/features/home/presentation/view_models/cubit/home_state.dart';
 import 'package:sams_app/features/home/presentation/views/widgets/mobile_course_card.dart';
 
 class CourseSliverList extends StatelessWidget {
@@ -9,15 +13,46 @@ class CourseSliverList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList.builder(
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 7),
-          child: MobileCourseCard(
-            role: UserRole.teacher,
-          ),
-        );
+    return BlocBuilder<HomeCubit, HomeState>(
+      buildWhen: (previous, current) {
+        return current is HomeSuccess ||
+            current is HomeFailure ||
+            current is HomeLoading;
+      },
+      builder: (context, state) {
+        if (state is HomeSuccess) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 7),
+                child: MobileCourseCard(
+                  role: UserRole.teacher,
+                  courseModel: state.courses[index],
+                ),
+              ),
+              childCount: state.courses.length,
+            ),
+          );
+        } else if (state is HomeFailure) {
+          return SliverToBoxAdapter(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUJoseKWLdz4o3GA4o_7P-b0cVClLE5o1RM0skmuTHYQ&s',
+                ),
+                const SizedBox(height: 10),
+                Text(state.errMessage),
+              ],
+            ),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: Padding(padding: EdgeInsets.symmetric(vertical: 20),
+            child: Center(child: AppAnimatedLoadingIndicator())),
+          );
+        }
       },
     );
   }
--- lib/features/home/presentation/views/widgets/courses_sliver_grid.dart
+++ lib/features/home/presentation/views/widgets/courses_sliver_grid.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:sams_app/features/home/data/models/home_course_model.dart';
-import 'package:sams_app/features/home/presentation/views/widgets/enum_user_role.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sams_app/core/enums/enum_user_role.dart';
+import 'package:sams_app/core/widgets/app_animated_loading_indicator.dart';
+import 'package:sams_app/features/home/presentation/view_models/cubit/home_cubit.dart';
+import 'package:sams_app/features/home/presentation/view_models/cubit/home_state.dart';
 import 'package:sams_app/features/home/presentation/views/widgets/web_course_card.dart';
 import 'package:sams_app/features/home/presentation/views/widgets/web_new_course_card.dart';
 
@@ -11,34 +14,58 @@ class CoursesSliverGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverPadding(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      sliver: SliverGrid(
-        delegate: SliverChildBuilderDelegate(
-          (context, index) {
-            if (index == 7) {
-              return const WebNewCourseCard(
-                role: UserRole.student,
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        if (state is HomeSuccess) {
+          return SliverPadding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        sliver: SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              if (index == state.courses.length-1) {
+                return const WebNewCourseCard(
+                  role: UserRole.teacher,
+                );
+              }
+              return  WebCourseCard(
+                course: state.courses[index],
+                role: UserRole.teacher,
               );
-            }
-            return const WebCourseCard(
-              course: HomeCourseModel(
-                courseName: 'Database',
-                courseCode: 'CS1010',
-                instructorName: 'Julie Watson',
-              ),
-              role: UserRole.student,
-            );
-          },
-          childCount: 8,
+            },
+            childCount: state.courses.length,
+          ),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            childAspectRatio: 301 / 240,
+          ),
         ),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          childAspectRatio: 301 / 240,
-        ),
-      ),
+      );
+        } else if (state is HomeFailure) {
+          return SliverToBoxAdapter(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUJoseKWLdz4o3GA4o_7P-b0cVClLE5o1RM0skmuTHYQ&s',
+                ),
+                const SizedBox(height: 10),
+                Text(state.errMessage),
+              ],
+            ),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+           child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Center(child: AppAnimatedLoadingIndicator()),
+            ),
+          );
+        }
+      },
     );
+    
   }
 }
