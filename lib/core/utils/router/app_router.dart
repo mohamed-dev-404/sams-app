61569f097d403a237ccdc0c8be600b26bc747d33
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 16:35:34 +0200

    feat(home): integrate BlocProvider into Router with automated data fetching.

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sams_app/core/enums/enum_user_role.dart';
 import 'package:sams_app/core/utils/router/build_route.dart';
 import 'package:sams_app/core/utils/router/routes_name.dart';
+import 'package:sams_app/core/utils/services/service_locator.dart';
+import 'package:sams_app/features/home/presentation/view_models/cubit/home_cubit.dart';
 import 'package:sams_app/features/home/presentation/views/create_course_view.dart';
 import 'package:sams_app/features/home/presentation/views/home_view.dart';
 
@@ -16,11 +20,20 @@ class AppRouter {
     initialLocation: RoutesName.splash,
     errorBuilder: (context, state) => const HomeView(),
     routes: [
+      //* HOME ROUTES
+      // Home view
       buildRoute(
         name: RoutesName.homeView,
         path: RoutesName.homeView,
-        builder: (context, state) => const HomeView(),
+        builder: (context, state) {
+          return BlocProvider(
+            create: (context) =>
+                getIt<HomeCubit>()..fetchMyCourses(role: UserRole.teacher),
+            child: const HomeView(),
+          );
+        },
       ),
+      // Create course view
       buildRoute(
         name: RoutesName.createCourseView,
         path: RoutesName.createCourseView,
