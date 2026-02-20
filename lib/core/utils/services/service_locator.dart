c2f3988552b3a62ff605506ec0783977bfd2f84c
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-20 15:36:24 +0200

    feat: improve service locator structure and register HomeCubit.

--- lib/core/utils/services/service_locator.dart
+++ lib/core/utils/services/service_locator.dart
@@ -4,15 +4,22 @@ import 'package:sams_app/core/network/api_consumer.dart';
 import 'package:sams_app/core/network/dio_consumer.dart';
 import 'package:sams_app/features/home/data/repos/home_repo.dart';
 import 'package:sams_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:sams_app/features/home/presentation/view_models/cubit/home_cubit.dart';
 
 GetIt getIt = GetIt.instance;
 
 void setupServiceLocator() {
+  getIt.registerLazySingleton<Dio>(() => Dio());
+  
   getIt.registerSingleton<ApiConsumer>(DioConsumer(Dio()));
 
 //! register HomeRepo
   getIt.registerLazySingleton<HomeRepo>(
     () => HomeRepoImpl( api:getIt<ApiConsumer>()),
+  );
+//!  New HomeCubit every time 
+getIt.registerFactory<HomeCubit>(
+    () => HomeCubit(getIt<HomeRepo>()),
   );
 
   //todo register other services and repositories as needed...
