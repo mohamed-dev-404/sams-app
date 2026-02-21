f8bc917fbbed4c2915fa42703fcfb8149a53faf5
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-21 06:38:30 +0200

    feat(home): impement joinCourse to use JoinCourseModel and add documentation for repo and repo impl.

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:sams_app/features/home/data/models/course_model.dart';
 import 'package:sams_app/core/enums/enum_user_role.dart';
 import 'package:sams_app/features/home/data/models/create_course_model.dart';
+import 'package:sams_app/features/home/data/models/join_course_model.dart';
 
 abstract class HomeRepo {
   Future<Either<String, List<CourseModel>>> fetchMyCourses({
@@ -10,6 +11,10 @@ abstract class HomeRepo {
 
   Future<Either<String, String>> createCourse({
     required CreateCourseModel course,
+  });
+
+  Future<Either<String, String>> joinCourse({
+    required JoinCourseModel model,
   });
   
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -2,8 +2,10 @@ import 'package:dartz/dartz.dart';
 import 'package:sams_app/core/enums/enum_user_role.dart';
 import 'package:sams_app/core/errors/exceptions/api_exception.dart';
 import 'package:sams_app/core/network/api_consumer.dart';
+import 'package:sams_app/core/utils/constants/api_endpoints.dart';
 import 'package:sams_app/features/home/data/models/course_model.dart';
 import 'package:sams_app/features/home/data/models/create_course_model.dart';
+import 'package:sams_app/features/home/data/models/join_course_model.dart';
 import 'package:sams_app/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
@@ -11,6 +13,11 @@ class HomeRepoImpl implements HomeRepo {
 
   HomeRepoImpl({required this.api});
 
+   //? Fetches courses based on the user role.
+  ///
+  /// Uses the role-specific endpoint to get the user's courses
+  /// and converts the response into a list of [CourseModel].
+  /// Returns either a failure message or a list of courses.
   @override
   Future<Either<String, List<CourseModel>>> fetchMyCourses({
     required UserRole role,
@@ -31,14 +38,18 @@ class HomeRepoImpl implements HomeRepo {
       return left(e.toString());
     }
   }
-  
+   //! Creates a new course.
+  ///
+  /// Sends [CreateCourseModel] data to the API.
+  /// Returns success message if created successfully,
+  /// otherwise returns an error message.
   @override
   Future<Either<String, String>> createCourse({
     required CreateCourseModel course,
   }) async {
     try {
       final response = await api.post(
-        'instructor/courses',
+        EndPoints.createCourse,
         data: course.toJson(),
       );
       return right(response['message'] ?? 'Course Created Successfully');
@@ -48,4 +59,28 @@ class HomeRepoImpl implements HomeRepo {
       return left(e.toString());
     }
   }
+
+   //* Joins a course using an invitation code.
+  ///
+  /// Sends [JoinCourseModel] data to the API.
+  /// Returns success message if joining succeeds,
+  /// otherwise returns an error message.
+  @override
+  Future<Either<String, String>> joinCourse({
+    required JoinCourseModel model,
+  }) async {
+    try {
+      var response = await api.post(
+        EndPoints.joinCourse,
+        data: model.toJson(),
+      );
+
+      return right(response['message'] ?? 'Joined successfully');
+    } on ApiException catch (e) {
+      return left(e.errorModel.errorMessage);
+    } catch (e) {
+      return left(e.toString());
+    }
+  }
+  
 }
