37a93b21a575900d3ff7253462c8f5bb87633e9a
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-21 04:29:35 +0200

    feat(home): add createCourse to repository and implementation.

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:sams_app/features/home/data/models/course_model.dart';
 import 'package:sams_app/core/enums/enum_user_role.dart';
+import 'package:sams_app/features/home/data/models/create_course_model.dart';
 
 abstract class HomeRepo {
   Future<Either<String, List<CourseModel>>> fetchMyCourses({
     required UserRole role,
   });
 
+  Future<Either<String, String>> createCourse({
+    required CreateCourseModel course,
+  });
+  
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:sams_app/core/enums/enum_user_role.dart';
 import 'package:sams_app/core/errors/exceptions/api_exception.dart';
 import 'package:sams_app/core/network/api_consumer.dart';
 import 'package:sams_app/features/home/data/models/course_model.dart';
+import 'package:sams_app/features/home/data/models/create_course_model.dart';
 import 'package:sams_app/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
@@ -24,6 +25,23 @@ class HomeRepoImpl implements HomeRepo {
           [];
 
       return right(courses);
+    } on ApiException catch (e) {
+      return left(e.errorModel.errorMessage);
+    } catch (e) {
+      return left(e.toString());
+    }
+  }
+  
+  @override
+  Future<Either<String, String>> createCourse({
+    required CreateCourseModel course,
+  }) async {
+    try {
+      final response = await api.post(
+        'instructor/courses',
+        data: course.toJson(),
+      );
+      return right(response['message'] ?? 'Course Created Successfully');
     } on ApiException catch (e) {
       return left(e.errorModel.errorMessage);
     } catch (e) {
