da812da6eed613eb4fdffc5631ae0160c79b48f4
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-22 09:09:52 +0200

    feat(home): add join states and joinCourse function in HomeCubit.

--- lib/features/home/presentation/view_models/cubit/home_cubit.dart
+++ lib/features/home/presentation/view_models/cubit/home_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sams_app/core/enums/enum_user_role.dart';
 import 'package:sams_app/features/home/data/models/create_course_model.dart';
+import 'package:sams_app/features/home/data/models/join_course_model.dart';
 import 'package:sams_app/features/home/data/repos/home_repo.dart';
 import 'package:sams_app/features/home/presentation/view_models/cubit/home_state.dart';
 
@@ -8,7 +9,12 @@ class HomeCubit extends Cubit<HomeState> {
   HomeCubit(this.homeRepo, {required this.role}) : super(HomeInitial());
 
   final HomeRepo homeRepo;
-  final UserRole role ;
+  final UserRole role;
+
+  //? Fetches the list of courses for the current user based on their [role].
+  ///
+  /// Emits [HomeLoading] while fetching, and either [HomeSuccess] with the courses
+  /// or [HomeFailure] with an error message.
   Future<void> fetchMyCourses({required UserRole role}) async {
     emit(HomeLoading());
 
@@ -19,7 +25,11 @@ class HomeCubit extends Cubit<HomeState> {
       (courses) => emit(HomeSuccess(courses)),
     );
   }
-
+  
+  //* Creates a new course as an Instructor.
+  ///
+  /// Takes a [CreateCourseModel] containing course details.
+  /// If successful, re-fetches the courses list and emits [CreateCourseSuccess].
   Future<void> createCourse({required CreateCourseModel course}) async {
     emit(CreateCourseLoading());
 
@@ -31,6 +41,25 @@ class HomeCubit extends Cubit<HomeState> {
         await fetchMyCourses(role: role);
 
         emit(CreateCourseSuccess(message));
+      },
+    );
+  }
+
+  //* Joins an existing course as a Student using an invitation code.
+  ///
+  /// Sends [JoinCourseModel] data to the repository.
+  /// If successful, re-fetches the user's courses and emits [JoinCourseSuccess].
+  Future<void> joinCourse({required JoinCourseModel model}) async {
+    emit(JoinCourseLoading());
+
+    final result = await homeRepo.joinCourse(model: model);
+
+    result.fold(
+      (failure) => emit(JoinCourseFailure(failure)),
+      (message) async {
+        await fetchMyCourses(role: role);
+
+        emit(JoinCourseSuccess(message));
       },
     );
   }
--- lib/features/home/presentation/view_models/cubit/home_state.dart
+++ lib/features/home/presentation/view_models/cubit/home_state.dart
@@ -1,33 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:sams_app/features/home/data/models/course_model.dart';
 
+//* Base state for Home feature.
+//* All Home states extend this class.
 @immutable
 sealed class HomeState {}
 
+/// Initial state before any action.
 final class HomeInitial extends HomeState {}
 
+//? Emitted while loading courses.
 final class HomeLoading extends HomeState {}
 
+//* Emitted when courses loaded successfully and emits list of courses.
 final class HomeSuccess extends HomeState {
   final List<CourseModel> courses;
   HomeSuccess(this.courses);
 }
 
+//! Emitted when loading courses fails and emits error message.
 final class HomeFailure extends HomeState {
   final String errMessage;
   HomeFailure(this.errMessage);
 }
 
+//* Base state for course actions .
+//* Used for create/join operations.
 sealed class CourseActionState extends HomeState {}
 
+//? Emitted while creating a course.
 final class CreateCourseLoading extends CourseActionState {}
 
+//* Emitted when course created successfully and emits success message.
 final class CreateCourseSuccess extends CourseActionState {
   final String message;
   CreateCourseSuccess(this.message);
 }
 
+//! Emitted when course creation fails and emits error message.
 final class CreateCourseFailure extends CourseActionState {
   final String errMessage;
   CreateCourseFailure(this.errMessage);
+}
+
+//? Emitted while joining a course.
+final class JoinCourseLoading extends CourseActionState {}
+
+//* Emitted when course joined successfully and emits success message.
+final class JoinCourseSuccess extends CourseActionState {
+  final String message;
+  JoinCourseSuccess(this.message);
+}
+
+//! Emitted when joining course fails and emits error message.
+final class JoinCourseFailure extends CourseActionState {
+  final String errMessage;
+  JoinCourseFailure(this.errMessage);
 }
