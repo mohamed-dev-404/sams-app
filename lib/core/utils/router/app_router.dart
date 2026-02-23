30b6061e921bbe1995e5ec8d665f645aa154d2fb
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-23 11:49:36 +0200

    feat(profile): update profile route with BlocProvider and initial fetch.

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sams_app/core/utils/router/build_route.dart';
 import 'package:sams_app/core/utils/router/routes_name.dart';
+import 'package:sams_app/core/utils/services/service_locator.dart';
 import 'package:sams_app/features/home/presentation/views/create_course_view.dart';
 import 'package:sams_app/features/home/presentation/views/home_view.dart';
+import 'package:sams_app/features/profile/data/repos/profile_repo.dart';
+import 'package:sams_app/features/profile/presentation/view_model/cubit/profile_cubit.dart';
 import 'package:sams_app/features/profile/presentation/views/profile_view.dart';
 
 class AppRouter {
@@ -36,8 +40,12 @@ class AppRouter {
       buildRoute(
         name: RoutesName.profileView,
         path: RoutesName.profileView,
-        builder: (context, state) => const ProfileView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) =>
+              ProfileCubit(getIt.get<ProfileRepo>())..getUserProfile(),
+          child: const ProfileView(),
         ),
+      ),
     ],
   );
 }
