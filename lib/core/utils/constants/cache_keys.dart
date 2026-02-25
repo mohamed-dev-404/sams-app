7e0e52eff85093d2d80080d921e3de4fa04e903b
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-25 09:29:28 +0200

    chore: implement CacheKeys for secure storage management

---
+++ lib/core/utils/constants/cache_keys.dart
@@ -0,0 +1,11 @@
+abstract class CacheKeys {
+  CacheKeys._();
+
+  static const String name = 'name';
+  static const String profilePic = 'profilePic';
+  static const String academicEmail = 'academicEmail';
+  static const String refreshToken = 'refreshToken';
+  static const String accessToken = 'accessToken';
+  static const String role = 'role';
+  static const String isStudent = 'isStudent';
+}
