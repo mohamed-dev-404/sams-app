aa1da7188b8cd4b5e76bc14408f3e4008e9f9d20
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-25 09:33:56 +0200

    chore: implement email masking function and update SnackBar color handling

--- lib/core/models/app_button_style_model.dart
+++ lib/core/models/app_button_style_model.dart
@@ -4,16 +4,16 @@ class AppButtonStyleModel {
   final String label;
   final Color? backgroundColor;
   final Color? textColor;
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
   final double? width;
-  final double?height;
+  final double? height;
 
   const AppButtonStyleModel({
     required this.label,
-     this.backgroundColor,
-     this.textColor,
-     required this.onPressed,
-     this.height,
-     this.width
+    this.backgroundColor,
+    this.textColor,
+    required this.onPressed,
+    this.height,
+    this.width,
   });
 }
