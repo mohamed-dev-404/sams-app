ea3b8d4cb6c28fb8848695a228da1607f7bafa05
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2026-02-27 13:06:29 +0200

    feat(router): implement nested routing for course details and add new tab views.

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sams_app/core/utils/router/build_route.dart';
 import 'package:sams_app/core/utils/router/routes_name.dart';
 import 'package:sams_app/core/widgets/general_error_page.dart';
+import 'package:sams_app/features/Grades/presentation/view/grades_tab_view.dart';
+import 'package:sams_app/features/announcements/presentation/view/announcements_tab_view.dart';
+import 'package:sams_app/features/assignments/presentation/view/assignments_tab_view.dart';
+import 'package:sams_app/features/course_code/presentation/view/course_code_tab_view.dart';
+import 'package:sams_app/features/course_details/presentation/view/course_details_view.dart';
+import 'package:sams_app/features/live_sessions/presentation/view/live_sessions_tab_view.dart';
+import 'package:sams_app/features/materials/presentation/view/materials_tab_view.dart';
+import 'package:sams_app/features/members_list/presentation/view/members_list_tab_view.dart';
+import 'package:sams_app/features/quizzes/presentation/view/quizzes_tab_view.dart';
 
 class AppRouter {
   AppRouter._();
@@ -11,14 +21,26 @@ class AppRouter {
 
   static final appRouter = GoRouter(
     navigatorKey: navigatorKey,
-    initialLocation: RoutesName.splash,
+    initialLocation: '${RoutesName.courseDetails}/${RoutesName.materials}',
     errorBuilder: (context, state) => const GeneralErrorPage(),
     routes: [
-      /// buildRoute(
-      ///   name: RoutesName.splash,
-      ///   path: RoutesName.splash,
-      ///   builder: (context, state) => const SplashView(),
-      /// ),
+      //* COURSE DETAILS (Nested Routing)
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return CourseDetailsView(navigationShell: navigationShell);
+        },
+        branches: [
+          _buildBranch(RoutesName.materials, const MaterialsTabView()),
+          _buildBranch(RoutesName.assignments, const AssignmentsTabView()),
+          _buildBranch(RoutesName.announcements, const AnnouncementsTabView()),
+          _buildBranch(RoutesName.grades, const GradesTabView()),
+          _buildBranch(RoutesName.quizzes, const QuizzesTabView()),
+          _buildBranch(RoutesName.liveSessions, const LiveSessionsTabView()),
+          _buildBranch(RoutesName.courseCode, const CourseCodeTabView()),
+          _buildBranch(RoutesName.membersList, const MembersListTabView()),
+        ],
+      ),
+
       /// buildRoute(
       ///   name: RoutesName.homeView,
       ///   path: RoutesName.homeView,
@@ -33,4 +55,15 @@ class AppRouter {
       //... more buildRoute calls
     ],
   );
+  static StatefulShellBranch _buildBranch(String path, Widget view) {
+    return StatefulShellBranch(
+      routes: [
+        buildRoute(
+          name: path,
+          path: '${RoutesName.courseDetails}/$path',
+          builder: (context, state) => view,
+        ),
+      ],
+    );
+  }
 }
--- lib/core/utils/router/routes_name.dart
+++ lib/core/utils/router/routes_name.dart
@@ -2,9 +2,17 @@ class RoutesName {
   RoutesName._();
 
   static const String splash = '/splash';
-  // static const String login = '/login';
-  // static const String register = '/register';
-  // static const String otp = '/otp';
-  // static const String home = '/home';
-  // ... other routes
+
+  static const String home = '/home';
+
+  static const String courseDetails = '/courseDetails';
+
+  static const String materials = 'materials';
+  static const String assignments = 'assignments';
+  static const String announcements = 'announcements';
+  static const String grades = 'grades';
+  static const String quizzes = 'quizzes';
+  static const String liveSessions = 'liveSessions';
+  static const String courseCode = 'courseCode';
+  static const String membersList = 'membersList';
 }
