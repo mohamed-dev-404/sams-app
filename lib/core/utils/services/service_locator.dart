20e82206e7ae26b7c25bee616991777e3a81d4b5
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-27 09:23:39 +0200

    feat(profile): register S3Service, ProfileRepo, and ProfileCubit in service locator.

--- lib/core/utils/services/service_locator.dart
+++ lib/core/utils/services/service_locator.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:sams_app/core/enums/enum_user_role.dart';
 import 'package:sams_app/core/network/api_consumer.dart';
 import 'package:sams_app/core/network/dio_consumer.dart';
+import 'package:sams_app/core/utils/services/s3_upload_service.dart';
 import 'package:sams_app/features/home/data/repos/home_repo.dart';
 import 'package:sams_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:sams_app/features/home/presentation/view_models/cubit/home_cubit.dart';
@@ -19,18 +20,26 @@ void setupServiceLocator() {
   getIt.registerLazySingleton<HomeRepo>(
     () => HomeRepoImpl( api:getIt<ApiConsumer>()),
   );
+  
 //!  New HomeCubit every time 
 getIt.registerFactory<HomeCubit>(
     () => HomeCubit(getIt<HomeRepo>(), role: UserRole.teacher),
   );
+
+//! register S3UploadService
+getIt.registerLazySingleton<S3UploadService>(() => S3UploadService());
+
 //! register ProfileRepo
 getIt.registerLazySingleton<ProfileRepo>(
-    () => ProfileRepoImpl(api: getIt<ApiConsumer>()),
+    () => ProfileRepoImpl(
+      api: getIt<ApiConsumer>(),
+       s3Service: getIt<S3UploadService>(),
+    ),
   );
-//! register ProfileRepo
+  
+//! register ProfileCubit
 getIt.registerFactory<ProfileCubit>(
     () => ProfileCubit(getIt<ProfileRepo>()),
   );
-
   //todo register other services and repositories as needed...
 }
