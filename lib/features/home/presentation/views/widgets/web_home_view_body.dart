8b840dc83f929d0c759e36d1b97462ce51746b5d
Nadia <nadiaashraff220@gmail.com>
2026-02-28 22:41:25 +0200

    refactor(core): refactor course header card.

--- lib/features/home/presentation/views/widgets/web_home_view_body.dart
+++ lib/features/home/presentation/views/widgets/web_home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:sams_app/core/models/course_header_card_model.dart';
 import 'package:sams_app/core/utils/styles/app_styles.dart';
-import 'package:sams_app/core/widgets/web_coures_header_card.dart';
+import 'package:sams_app/core/widgets/web_course_header_card.dart';
 import 'package:sams_app/features/home/presentation/views/widgets/courses_sliver_grid.dart';
 import 'package:sams_app/features/home/presentation/views/widgets/web_home_header.dart';
 
