66d3dc65fc38f37c9d6fff12507ec9c74e846ac4
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-02-28 14:37:09 +0200

    feat(profile): implement interactive ImagePreviewDialog for profile photos with animation.

---
+++ lib/features/profile/presentation/views/widgets/image_preview_dialog.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:sams_app/core/utils/colors/app_colors.dart';
+import 'package:sams_app/core/utils/configs/size_config.dart';
+
+class ImagePreviewDialog extends StatelessWidget {
+  final String imageUrl;
+
+  const ImagePreviewDialog({super.key, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    final mobileSize = SizeConfig.isMobile(context);
+
+    return Center(
+      child: Container(
+        width: mobileSize
+            ? double.infinity
+            : MediaQuery.of(context).size.width * 0.5,
+        height: mobileSize
+            ? double.infinity
+            : MediaQuery.of(context).size.height * 0.7,
+        padding: mobileSize ? EdgeInsets.zero : const EdgeInsets.all(20),
+        child: Material(
+          color: Colors.transparent,
+          child: Stack(
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
+                  child: Container(color: Colors.transparent),
+                ),
+              ),
+              Center(
+                child: InteractiveViewer(
+                  minScale: 0.5,
+                  maxScale: 4.0,
+                  child: Hero(
+                    tag: imageUrl,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(!mobileSize ? 16 : 0),
+                      child: CachedNetworkImage(
+                        imageUrl: imageUrl,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+ static void open(BuildContext context, String imageUrl) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: '',
+      barrierColor: AppColors.black.withValues(alpha: 0.7),
+      transitionDuration: const Duration(milliseconds: 200),
+      pageBuilder: (_, __, ___) => ImagePreviewDialog(imageUrl: imageUrl),
+    );
+  }
+}
