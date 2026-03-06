d5179bc2d2243f5a421e7cf2e7adc5554d27d37a
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-03-07 01:27:00 +0200

    feat(home): update home cubit to support eimit offline data  with messageand reactive fetching.

--- lib/features/profile/presentation/view_model/cubit/profile_cubit.dart
+++ lib/features/profile/presentation/view_model/cubit/profile_cubit.dart
@@ -1,27 +1,65 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:async';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:sams_app/features/profile/data/models/user_model.dart';
 import 'package:sams_app/features/profile/data/repos/profile_repo.dart';
+import 'package:sams_app/core/utils/mixins/cubit_message_mixin.dart';
 import 'package:sams_app/features/profile/presentation/view_model/cubit/profile_state.dart';
 
-class ProfileCubit extends Cubit<ProfileState> {
+class ProfileCubit extends HydratedCubit<ProfileState> with  CubitMessageMixin
+ {
   final ProfileRepo profileRepo;
 
   ProfileCubit(this.profileRepo) : super(ProfileInitial());
+
 
  //? Fetches the user's profile data.
   ///
   /// Emits [ProfileLoading] while fetching, and either [ProfileSuccess] with the user's profile
   /// or [ProfileFailure] with an error message.
   Future<void> getUserProfile() async {
-    emit(ProfileLoading());
+
+   if (state is! ProfileSuccess) {
+      emit(ProfileLoading());
+    }
 
     final result = await profileRepo.getUserProfile();
 
     result.fold(
-      (failure) => emit(ProfileFailure(failure)),
+      (failure) {
+        if (state is ProfileSuccess) {
+          emitMessage(failure);
+        } else {
+          emit(ProfileFailure(failure));
+        }
+      },
       (userModel) => emit(ProfileSuccess(userModel)),
     );
   }
+
+ @override
+  Future<void> close() {
+    closeMessages();
+    return super.close();
+  }
+  @override
+  Map<String, dynamic>? toJson(ProfileState state) {
+    if (state is ProfileSuccess) {
+      return state.userModel.toMap(); 
+    }
+    return null;
+  }
+
+  @override
+  ProfileState? fromJson(Map<String, dynamic> json) {
+    try {
+      final user = UserModel.fromMap(json);
+      return ProfileSuccess(user);
+    } catch (_) {
+      return null;
+    }
+  }
+
 
   //? Uploads the user's profile picture.
   ///
@@ -42,3 +80,4 @@ class ProfileCubit extends Cubit<ProfileState> {
     );
   }
 }
+
