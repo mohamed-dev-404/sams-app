806c2f9af4e4f24de4455312a3cc38dbc529863b
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-03-08 00:15:59 +0200

    feat(home): add delete course endpoint and user role extension for dynamic routing.

--- lib/core/utils/constants/api_endpoints.dart
+++ lib/core/utils/constants/api_endpoints.dart
@@ -3,14 +3,15 @@ class EndPoints {
 
   //? EndPoints
 
-   //! --- Home  --- ;
-   //* Instructor endpoints
+  //! --- Home  --- ;
+  //* Instructor endpoints
   static const String createCourse = 'instructor/courses';
   static const String getMyCreatedCourses = 'instructor/courses/me';
+  static String deleteCourse(String courseId) => 'instructor/courses/$courseId';
 
   //* Enrollment endpoints
   static const String getMyJoinedCourses = 'enrollments/me';
-  static const String joinCourse = 'enrollments'; 
+  static const String joinCourse = 'enrollments';
   static String unenrollCourse(String courseId) =>
       'enrollments/my-courses/$courseId';
 
@@ -19,5 +20,4 @@ class EndPoints {
   static const String updateProfile = 'users/profile';
   static const String createUploadUrl = 'users/profile-picture/presigned-url';
   static const String saveProfilePic = 'users/profile-picture';
-
 }
