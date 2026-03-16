869e0acfc42dca6b87ded4a43a3a795b32df088b
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-03-16 21:28:25 +0200

    feat: add all api endpoints for quiz feature.

--- lib/core/utils/constants/api_endpoints.dart
+++ lib/core/utils/constants/api_endpoints.dart
@@ -30,4 +30,31 @@ class EndPoints {
   static const String updateProfile = 'users/profile';
   static const String createUploadUrl = 'users/profile-picture/presigned-url';
   static const String saveProfilePic = 'users/profile-picture';
+
+  //? --- Quizzes --- ;
+
+  //* Discovery Flow (Student/General)
+  static String getCourseQuizzes(String courseId) => 'courses/$courseId/quizzes';
+  static String getQuizDetails(String quizId) => 'quizzes/$quizId';
+  static String getQuizQuestions(String quizId) => 'quizzes/$quizId/questions';
+  
+  //* Student Flow
+  static String submitQuiz(String quizId) => 'quizzes/$quizId/submit';
+  
+  //* Instructor Flow - Quiz CRUD
+  static String createQuiz(String courseId) => 'instructor/courses/$courseId/quizzes';
+  static String updateQuiz(String quizId) => 'instructor/quizzes/$quizId';
+  static String deleteQuiz(String quizId) => 'instructor/quizzes/$quizId';
+  static String toggleQuizPublished(String quizId) => 'instructor/quizzes/$quizId/toggle-published';
+  
+  //* Instructor Flow - Questions CRUD
+  static String createQuestion(String quizId) => 'instructor/quizzes/$quizId/questions';
+  static String updateQuestion(String questionId) => 'instructor/questions/$questionId';
+  static String deleteQuestion(String questionId) => 'instructor/questions/$questionId';
+
+  //* Instructor Flow - Submissions & Grading
+  static String getQuizSubmissions(String quizId) => 'instructor/quizzes/$quizId/submissions';
+  static String getSubmissionDetails(String submissionId) => 'instructor/submissions/$submissionId';
+  static String gradeQuestion(String submissionId, String questionId) => 'instructor/submissions/$submissionId/questions/$questionId';
+
 }
