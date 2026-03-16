91e9875e4e81bc34eec853f788c46cb52a3fc05c
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-03-16 07:47:46 +0200

    feat(quizzes): Add a new Lottie animations for quiz start and empty states to provide visual feedback.

--- lib/core/utils/assets/app_lottie.dart
+++ lib/core/utils/assets/app_lottie.dart
@@ -2,4 +2,6 @@ class AppLottie {
   AppLottie._();
 
   static const String quizSubmissions = 'assets/lottie/quiz_submissions.json';
+  static const String startQuiz = 'assets/lottie/start_quiz.json';
+  static const String empty = 'assets/lottie/empty.json';
 }
