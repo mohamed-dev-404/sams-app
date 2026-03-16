3e3c0698779a09e24b0e065e3e9f09e128902ee7
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-03-16 20:26:07 +0200

    feat(quiz): init QuizRepository interface and its initial implementation for quiz feature.

---
+++ lib/features/quizzes/data/repos/quiz_repository.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+
+abstract class QuizRepository {
+  // --- Discovery Flow ---
+  Future<Either<String, List<dynamic>>> getQuizzesForCourse(String courseId);
+  Future<Either<String, dynamic>> getQuizDetails(String quizId);
+  Future<Either<String, List<dynamic>>> getQuizQuestions(String quizId);
+
+  // --- Instructor Flow: Quiz CRUD ---
+  Future<Either<String, String>> createQuiz(String courseId, Map<String, dynamic> data);
+  Future<Either<String, String>> updateQuiz(String quizId, Map<String, dynamic> data);
+  Future<Either<String, String>> deleteQuiz(String quizId);
+  Future<Either<String, String>> toggleQuizPublished(String quizId);
+
+  // --- Instructor Flow: Questions CRUD ---
+  Future<Either<String, String>> addQuestion(String quizId, Map<String, dynamic> data);
+  Future<Either<String, String>> updateQuestion(String questionId, Map<String, dynamic> data);
+  Future<Either<String, String>> deleteQuestion(String questionId);
+
+  // --- Student Flow: Taking Quizzes ---
+  Future<Either<String, String>> submitQuiz(String quizId, Map<String, dynamic> data);
+
+  // --- Instructor Flow: Grading ---
+  Future<Either<String, List<dynamic>>> getQuizSubmissions(String quizId);
+  Future<Either<String, dynamic>> getSubmissionDetails(String submissionId);
+  Future<Either<String, String>> gradeSubmissionQuestion(String submissionId, String questionId, Map<String, dynamic> data);
+}
---
+++ lib/features/quizzes/data/repos/quiz_repository_impl.dart
@@ -0,0 +1,103 @@
+import 'package:dartz/dartz.dart';
+import 'package:sams_app/core/network/api_consumer.dart';
+import 'package:sams_app/features/quizzes/data/repos/quiz_repository.dart';
+
+class QuizRepositoryImpl implements QuizRepository {
+  final ApiConsumer api;
+
+  QuizRepositoryImpl({required this.api});
+
+  // --- Discovery Flow ---
+
+  @override
+  Future<Either<String, List<dynamic>>> getQuizzesForCourse(String courseId) async {
+    // TODO: implement getQuizzesForCourse
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, dynamic>> getQuizDetails(String quizId) async {
+    // TODO: implement getQuizDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, List<dynamic>>> getQuizQuestions(String quizId) async {
+    // TODO: implement getQuizQuestions
+    throw UnimplementedError();
+  }
+
+  // --- Instructor Flow: Quiz CRUD ---
+
+  @override
+  Future<Either<String, String>> createQuiz(String courseId, Map<String, dynamic> data) async {
+    // TODO: implement createQuiz
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> updateQuiz(String quizId, Map<String, dynamic> data) async {
+    // TODO: implement updateQuiz
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> deleteQuiz(String quizId) async {
+    // TODO: implement deleteQuiz
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> toggleQuizPublished(String quizId) async {
+    // TODO: implement toggleQuizPublished
+    throw UnimplementedError();
+  }
+
+  // --- Instructor Flow: Questions CRUD ---
+
+  @override
+  Future<Either<String, String>> addQuestion(String quizId, Map<String, dynamic> data) async {
+    // TODO: implement addQuestion
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> updateQuestion(String questionId, Map<String, dynamic> data) async {
+    // TODO: implement updateQuestion
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> deleteQuestion(String questionId) async {
+    // TODO: implement deleteQuestion
+    throw UnimplementedError();
+  }
+
+  // --- Student Flow: Taking Quizzes ---
+
+  @override
+  Future<Either<String, String>> submitQuiz(String quizId, Map<String, dynamic> data) async {
+    // TODO: implement submitQuiz
+    throw UnimplementedError();
+  }
+
+  // --- Instructor Flow: Grading ---
+
+  @override
+  Future<Either<String, List<dynamic>>> getQuizSubmissions(String quizId) async {
+    // TODO: implement getQuizSubmissions
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, dynamic>> getSubmissionDetails(String submissionId) async {
+    // TODO: implement getSubmissionDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> gradeSubmissionQuestion(String submissionId, String questionId, Map<String, dynamic> data) async {
+    // TODO: implement gradeSubmissionQuestion
+    throw UnimplementedError();
+  }
+}
