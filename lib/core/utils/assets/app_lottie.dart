0ac26023b265582147a369a0b5312c01e2e951ce
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-03-22 16:30:38 +0200

    feat(take-quiz): implement visually stunning animated success layout.

--- lib/core/utils/assets/app_lottie.dart
+++ lib/core/utils/assets/app_lottie.dart
@@ -4,4 +4,6 @@ class AppLottie {
   static const String quizSubmissions = 'assets/lottie/quiz_submissions.json';
   static const String startQuiz = 'assets/lottie/start_quiz.json';
   static const String empty = 'assets/lottie/empty.json';
+  static const String success = 'assets/lottie/success.json';
+  static const String fireWorks = 'assets/lottie/fire_works.json';
 }
