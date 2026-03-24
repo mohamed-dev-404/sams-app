798236c54a8aa53e4e40439c6f1fec2108007c99
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-03-24 15:27:31 +0200

    chore: Add new Lottie animations for delete and edit actions

--- lib/core/utils/assets/app_lottie.dart
+++ lib/core/utils/assets/app_lottie.dart
@@ -3,4 +3,6 @@ class AppLottie {
 
   static const String empty = 'assets/lottie/empty.json';
   static const String uploadFiles = 'assets/lottie/upload_files.json';
+  static const String delete = 'assets/lottie/delete.json';
+  static const String edit = 'assets/lottie/edit.json';
 }
