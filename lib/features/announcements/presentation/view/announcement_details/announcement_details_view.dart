e557f621f504cdf4da704a382d6fa348ce85f854
Nadia <nadiaashraff220@gmail.com>
2026-04-09 15:20:39 +0200

    feat(announcement): implement announcement details view structure

---
+++ lib/features/announcements/presentation/view/announcement_details/announcement_details_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sams_app/core/widgets/shared/adaptive_layout.dart';
+import 'package:sams_app/features/announcements/presentation/view/announcement_details/widget/mobile/announcement_details_mobile_view.dart';
+import 'package:sams_app/features/announcements/presentation/view/announcement_details/widget/web/announcement_details_web_view.dart';
+
+class AnnouncementDetailsView extends StatelessWidget {
+  const AnnouncementDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveLayout(
+      mobileLayout: (BuildContext context) {
+        return const AnnouncementDetailsMobileView();
+      },
+      webLayout: (BuildContext context) {
+        return const AnnouncementDetailsWebView();
+      },
+    );
+  }
+}
