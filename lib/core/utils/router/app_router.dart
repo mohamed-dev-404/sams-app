4b4a1c4570e98980f3bd56783fdd11bbf1529b47
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-04-20 13:59:02 +0200

    fix(naming): rename assignment files and fix imports.

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -10,7 +10,7 @@ import 'package:sams_app/core/utils/services/service_locator.dart';
 import 'package:sams_app/core/widgets/shared/general_error_page.dart';
 import 'package:sams_app/features/Grades/presentation/view/grades_tab_view.dart';
 import 'package:sams_app/features/announcements/presentation/view/announcements_tab_view.dart';
-import 'package:sams_app/features/assignments/presentation/view/assiment_tap_view/assignments_tab_view.dart';
+import 'package:sams_app/features/assignments/presentation/view/assignment_tap_view/assignments_tab_view.dart';
 // Repos & Cubits
 import 'package:sams_app/features/auth/data/repos/auth_repo.dart';
 import 'package:sams_app/features/auth/presentation/view_models/login_cubit/login_cubit.dart';
